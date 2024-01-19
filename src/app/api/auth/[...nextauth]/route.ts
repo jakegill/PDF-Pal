f8ef21c327aa49b66733a15d41817115674f4601
Jake Gill <jake.w.g21@gmail.com>
2024-01-19 09:57:39 -0500

    Init postgres & prisma

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,53 @@
+import NextAuth from "next-auth/next";
+import { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "@/lib/prisma";
+import bcrypt from "bcrypt";
+import { User } from "@prisma/client";
+
+export const authOptions: AuthOptions = {
+	pages: {
+		signIn: "/auth/signin",
+	},
+	providers: [
+		CredentialsProvider({
+			name: "Credentials",
+			credentials: {
+				username: { label: "Username", type: "text" },
+				password: { label: "Password", type: "password" },
+			},
+			async authorize(credentials) {
+				const user = await prisma.user.findUnique({
+					where: {
+						email: credentials?.username as string,
+					},
+				});
+				if (!user) throw new Error("User not found.");
+
+				const isValidPassword = await bcrypt.compare(credentials?.password as string, user.password)
+				if (!isValidPassword) throw new Error("Invalid credentials.");
+
+				const { password, ...userWithoutPassword } = user;
+				return userWithoutPassword;
+			},
+		}),
+	],
+	callbacks: {
+		async session({token, session}) { 
+			session.user = token.user as User;
+			return session;
+		},
+		async jwt({token, user}) {
+			if (user) token.user = user as User;
+			return token;
+		},
+	},
+	session: {
+		strategy: "jwt",
+		maxAge: 24 * 60 * 60, // 24 hours
+	}
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
---
+++ src/prisma/migrations/20240119145633_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email" TEXT,
+    "emailVerified" TIMESTAMP(3),
+    "image" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
